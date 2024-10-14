fca5466bd10bd9b3e0fb2fb9327f3451ccb4f794
k0yusuf <147905690+k0yusuf@users.noreply.github.com>
2024-10-14 22:31:30 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,6 +4,6 @@ import joblib  # To load the pre-trained model
 import numpy as np
 
 # Load the dataset containing player stats
-df = pd.read_csv('https://raw.githubusercontent.com/k0yusuf/FYP/refs/heads/master/NBA%20Dataset%20Final%20CSV.csv')
+df = pd.read_csv('https://raw.githubusercontent.com/k0yusuf/FYP/refs/heads/master/df_2024.csv')
 
 df

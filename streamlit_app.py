6888a644927f0ab94655e676d0b54e76fe731d0d
k0yusuf <147905690+k0yusuf@users.noreply.github.com>
2024-10-22 21:36:54 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -93,7 +93,7 @@ else:
         st.dataframe(average_stats)
 
     # Load the SVM model
-    SVM_model = pickle.load(open('svm_model.sav', 'rb'))
+    SVM_model = pickle.load(open('svm_model (1).pkl', 'rb'))
 
     # Prediction of the season outcome using the pre-trained model
     prediction = SVM_model.predict([average_stats])

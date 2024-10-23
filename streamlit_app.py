64854e92b0b53be36d70b6ec7f0a4671cabad745
k0yusuf <147905690+k0yusuf@users.noreply.github.com>
2024-10-23 21:38:18 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,6 @@ import pandas as pd
 import joblib  # To load the pre-trained model
 import numpy as np
 import pickle
-pip install keras tensorflow
 # Load the dataset containing player stats
 df = pd.read_csv('https://raw.githubusercontent.com/k0yusuf/FYP/refs/heads/master/df_2024.csv')
 

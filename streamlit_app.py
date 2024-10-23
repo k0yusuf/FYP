9f57dd360a461bf06c1d534c7ac2cf4f3e3a62a8
k0yusuf <147905690+k0yusuf@users.noreply.github.com>
2024-10-23 22:21:23 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -99,7 +99,7 @@ else:
     SVM_model = joblib.load('rf_model.joblib')
     #scaler = joblib.load('svm_scaler.joblib')  # Load the scaler used in training
     scaler = StandardScaler()
-    average_stats_scaled = scaler.fit_transform([average_stats])  # Apply scaling
+    average_stats_scaled = scaler.transform([average_stats])  # Apply scaling
 
     # Prediction of the season outcome using the pre-trained model
     prediction = SVM_model.predict([average_stats_scaled])

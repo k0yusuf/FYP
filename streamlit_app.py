6204dbd9d995ececd42fba067906f8dd0ba3a243
k0yusuf <147905690+k0yusuf@users.noreply.github.com>
2024-10-31 15:48:50 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -119,7 +119,7 @@ else:
             return SVM_model.predict_proba(X)
 
     # Use KernelExplainer for SHAP
-        shap_explainer = shap.KernelExplainer(predict_proba_shap, shap.kmeans(scaler.transform(df.drop(['Player', 'Season', 'Season Outcome'], axis=1).values), 10))
+        shap_explainer = shap.KernelExplainer(predict_proba_shap, shap.kmeans(scaler.transform(df.drop(['Player', 'Season', 'Season Outcome', 'Team','Offense Position', 'Offensive Archetype', 'Defensive Role'], axis=1).values), 10))
         shap_values = shap_explainer.shap_values(scaled_average_stats)
 
     # Plot SHAP values

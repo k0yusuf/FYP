ca471e0a0cac29f1cc7005f54095e8974a4643f2
k0yusuf <147905690+k0yusuf@users.noreply.github.com>
2024-11-07 00:02:35 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -77,7 +77,7 @@ else:
 
     # Filter selected player stats and calculate average
     selected_players_df = df[df['Player'].isin(selected_players)]
-    average_stats = selected_players_df.mean(numeric_only=True).drop(['Season', 'Season Outcome'], errors='ignore')
+    average_stats = selected_players_df.mean(numeric_only=True).values.reshape(1, -1)
     average_stats_df = pd.DataFrame(average_stats) # Convert Series to DataFrame with one row
 
     # Display average stats

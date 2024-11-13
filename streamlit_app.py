605a976011eab17ea38f57baab057e0a28fbb541
k0yusuf <147905690+k0yusuf@users.noreply.github.com>
2024-11-13 20:05:32 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -215,8 +215,9 @@ else:
     
     # Calculate average stats only for numeric columns, ignoring non-numeric ones
     numeric_columns = selected_players_df.select_dtypes(include=[np.number]).columns
+    numeric_columns.drop['Season']
     average_stats_to_display = selected_players_df[numeric_columns.mean().drop(['Season', 'Season Outcome'], errors='ignore')].values
-    average_stats = selected_players_df.mean(numeric_only=True).drop(['Season', 'Season Outcome'], errors='ignore').values.reshape(1, -1)    
+    average_stats = selected_players_df[numeric_columns].mean().values    
     # Display average stats in an attractive format
     st.markdown("### 📊 Team Average Statistics")
     cols = st.columns(4)
